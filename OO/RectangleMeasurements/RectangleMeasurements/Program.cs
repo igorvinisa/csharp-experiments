00aab6f3672c45b4b1dcb17eb987b1977f2ba1b1
igorvinisa <igor.offn@gmail.com>
2025-09-28 18:26:10 -0300

    Adding rectangle calculator

---
+++ OO/RectangleMeasurements/RectangleMeasurements/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace RectangleMeasurements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            
+            Console.WriteLine("Entre com a largura e altura do retangulo: ");
+
+            rectangle.Base = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            rectangle.Height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(rectangle);
+        }
+    }
+}
---
+++ OO/RectangleMeasurements/RectangleMeasurements/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization; 
+using System.Threading.Tasks;
+
+namespace RectangleMeasurements
+{
+    public class Rectangle
+    {
+        public double Base;
+
+        public double Height;
+
+        public double Area()
+        {
+            return Base * Height;
+        }   
+
+        public double Perimeter()
+        {
+            return 2 * (Base + Height);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Base * Base + Height * Height);
+        }   
+
+        public override string ToString()
+        {
+            return $"Area: {Area().ToString("F2", CultureInfo.InvariantCulture)}, Perimeter: {Perimeter().ToString("F2", CultureInfo.InvariantCulture)}, Diagonal: {Diagonal().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
