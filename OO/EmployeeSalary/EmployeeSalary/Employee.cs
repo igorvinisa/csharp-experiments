1c25a3dd03f3fa2e094c45eea1e811d4e7ce4506
igorvinisa <igor.offn@gmail.com>
2025-09-28 18:47:25 -0300

    Adding IncreasedSalary percentage program in repos

---
+++ OO/EmployeeSalary/EmployeeSalary/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSalary
+{
+    public class Employee
+    {
+        public string Name;
+
+        public double GrossSalary;
+
+        public double Tax;
+
+        public double NetPay()
+        {
+            return GrossSalary - Tax;
+        }
+
+        public void IncreaseSalary(double percentage)
+        {
+            GrossSalary += GrossSalary * percentage / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", $ "
+                + NetPay().ToString("F2", System.Globalization.CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ OO/EmployeeSalary/EmployeeSalary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+namespace EmployeeSalary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            Console.Write("Name: ");
+            employee.Name = Console.ReadLine();
+            Console.Write("Gross Salary: ");
+            employee.GrossSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Tax: ");
+            employee.Tax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine(employee);
+
+            Console.WriteLine();
+            Console.WriteLine("Which percentage to increase salary? ");
+
+            double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            employee.IncreaseSalary(percentage);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Updated data: " + employee);
+        }
+    }
+}
