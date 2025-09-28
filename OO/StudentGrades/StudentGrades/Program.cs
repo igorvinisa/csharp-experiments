896a1502791dd41c64cf9e3b77cf570bb9a2d5aa
igorvinisa <igor.offn@gmail.com>
2025-09-28 19:34:32 -0300

    grade calculate to approve student

---
+++ OO/StudentGrades/StudentGrades/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace StudentGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            Console.Write("Nome do Aluno: ");
+            student.Name = Console.ReadLine();
+
+            Console.WriteLine("Digite as tres notas do aluno");
+            student.FirstGrade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.SecondGrade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.ThirdGrade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("");
+            Console.WriteLine(student);
+        }
+    }
+}
---
+++ OO/StudentGrades/StudentGrades/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace StudentGrades
+{
+    public class Student
+    {
+        private double MinimumGrade = 60.0;
+        
+        public string Name;
+
+        public double FirstGrade;
+        
+        public double SecondGrade;
+        
+        public double ThirdGrade;
+
+
+        public double FinalResult()
+        {
+            return (FirstGrade + SecondGrade + ThirdGrade);
+        }
+
+        public double MissingPoints()
+        {
+            return MinimumGrade - FinalResult();
+        }
+
+        public override string ToString()
+        {
+            if(FinalResult() >= MinimumGrade)
+                return string.Format("Final Result = {0}, Student Approved", FinalResult().ToString("F2", CultureInfo.InvariantCulture));
+            else
+                return string.Format("Final Result = {0}, Student Reproved. {1} Missing Points",
+                                    FinalResult().ToString("F2", CultureInfo.InvariantCulture), 
+                                    MissingPoints().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
