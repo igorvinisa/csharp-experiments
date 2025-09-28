7981e3ed3df7f0671ad695a1c346dbe4a002bf02
igorvinisa <igor.offn@gmail.com>
2025-09-28 18:09:35 -0300

    Currency Converter to OO praticles

---
+++ OO/CurrencyConverter/CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    public class CurrencyConverter
+    {
+        public static double Iof = 0.06;
+
+        public static double ConvertToDollarWithIOF(double rate, double amount)
+        {
+            return rate * amount * Iof + rate * amount;
+        }
+    }
+}
---
+++ OO/CurrencyConverter/CurrencyConverter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace CurrencyConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotacao do dolar? ");
+
+            double rate = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dolares voce ira comprar? ");
+
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = CurrencyConverter.ConvertToDollarWithIOF(rate, amount);
+
+            Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
