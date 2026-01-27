76746e46936ad339123d4f792bc3a299b162d69b
igorvinisa <igor.offn@gmail.com>
2026-01-26 23:48:59 -0300

    withdraw and deposit BankAccount functions, constructors, properties and encapsulation

---
+++ OO/BankAccount/BankAccount/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class Account
+    {
+        private string _number;
+
+        private const decimal WithdrawalFee = 5.00m;
+        
+        public decimal Balance { get; private set; }
+
+        public string HolderName { get; set; }
+
+        public Account(string number, string holderName)
+        {
+            _number = number;
+            HolderName = holderName;
+        }
+
+        public void Deposit (decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw (decimal amount)
+        {
+            Balance -= amount + WithdrawalFee;
+        }
+
+        public override string ToString()
+        {
+            return $"Account: {_number}, Holder: {HolderName}, Balance: ${Balance:F2}";
+        }
+    }
+}
---
+++ OO/BankAccount/BankAccount/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the account number: ");
+            string accountNumber = Console.ReadLine();
+
+            Console.Write("Enter the account holder's name: ");
+            string holderName = Console.ReadLine();
+
+            Console.Write("Is there an initial deposit (y/n)? ");
+            string initialDepositResponse = Console.ReadLine();
+
+            Account account = new Account(accountNumber, holderName);
+
+            if (initialDepositResponse.ToLower() == "y")
+            {
+                Console.Write("Enter the initial deposit amount: ");
+                decimal initialDeposit = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account.Deposit(initialDeposit);
+                Console.WriteLine("Account created:");
+                Console.WriteLine(account);
+
+                Console.Write("Enter a amount for withdraw: ");
+                decimal withdrawInput = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account.Withdraw(withdrawInput);
+                Console.WriteLine(account);
+            }
+            else
+            {
+                Console.WriteLine("Account created:");
+                Console.WriteLine(account);
+            }
+        }
+    }
+}
