4ce78e58d54b6e7414e8d021d0fa0546ebc1d6bf
igorvinisa <igor.offn@gmail.com>
2026-02-02 23:13:49 -0300

    two different approaches to solving the rental problem

--- OO/StudentRent/StudentRent/Program.cs
+++ OO/StudentRent/StudentRent/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            /*1# Solution - Rental and room approach*/
+
             Console.Write("How many rooms will be rent? ");
             int n = int.Parse(Console.ReadLine());
 
@@ -29,6 +31,34 @@
                 if (rooms[i] != null)
                 {
                     Console.WriteLine($"{i}: {rooms[i].Rent.Name}, {rooms[i].Rent.Mail}");
+                }
+            }
+
+
+            /*2# Solution - Student Approach*/
+
+            Student[] students = new Student[10];
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Rent #{i}:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string mail = Console.ReadLine();
+                Console.Write("Room number: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+                students[roomNumber] = new Student(name, mail);
+            }
+
+            Console.WriteLine($"Busy  rooms#:");
+            
+            for (int i = 0; i < students.Length; i++)
+            {
+                if (students[i] != null)
+                {
+                    Console.WriteLine($"{i}: {students[i]}");
                 }
             }
 
---
+++ OO/StudentRent/StudentRent/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRent
+{
+    public  class Student
+    {
+        public string Name { get; set; }
+
+        public string Mail { get; set; }
+
+        public Student(string name, string mail)
+        {
+            Name = name;
+            Mail = mail;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Mail}";
+        }
+    }
+}
