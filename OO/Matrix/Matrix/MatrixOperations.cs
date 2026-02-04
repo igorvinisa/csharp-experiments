28f3e55062cc81d2eeccbacc8f8040e834d8da85
igorvinisa <igor.offn@gmail.com>
2026-02-04 20:22:17 -0300

    Learning about matrices

---
+++ OO/Matrix/Matrix/MatrixOperations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public static class MatrixOperations
+    {
+        public static int[] GetDiagonalElements(int[,] matrix) {
+
+            int[] diagonalElements = new int[matrix.GetLength(0)];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        diagonalElements[i] = matrix[i, j];
+                    }
+                }
+            }
+
+            return diagonalElements;
+        }
+
+        public static int GetNegativeElementsCount(int[,] matrix) {
+            
+            int negativeCount = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] < 0)
+                    {
+                        negativeCount++;
+                    }
+                }
+            }
+            return negativeCount;
+        }
+    }
+}
---
+++ OO/Matrix/Matrix/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("specify the order of a matrix N: ");
+            int order = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[order, order];
+
+            Console.WriteLine("Matrix Numbers: ");
+            for (int i = 0; i < order; i++)
+            {
+                string[] elementsLine = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < order; j++)
+                {
+                    matrix[i, j] = int.Parse(elementsLine[j]);
+                }
+            }
+
+            int[] diagonalElements = MatrixOperations.GetDiagonalElements(matrix);
+
+            Console.WriteLine("Diagonal Elements: ");
+            for (int i = 0; i < diagonalElements.Length; i++)
+            {
+                Console.Write(diagonalElements[i] + " ");
+            }
+
+            Console.WriteLine("Negative numbers count: " + MatrixOperations.GetNegativeElementsCount(matrix));
+
+        }
+    }
+}
